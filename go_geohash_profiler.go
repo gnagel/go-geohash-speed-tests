dbb8130b699874eafd50bff3bae2f691abc1d3a4
Glenn Nagel <glenn@mercury-wireless.com>
2013-07-25 11:31:18 -0400

    Working on speed tests ...

--- go_geohash_profiler.go
+++ go_geohash_profiler.go
@@ -14,12 +14,13 @@ var geostr = "ww8p1r4t8"
 var neighbor = "dqcjq"
 var directions = [2]int{1, 0}
 var num_loops int = 1000 * 1000
-var format_tag = "[%-30s][%-12s]\t MS Per/Call %.4f (ms)\t Total for %d Calls = %5f (ms)\n"
+var format_tag = "[%-30s][%-12s]\t NS Per/Call %10.d (ns)\t Total for %d Calls = %5f (ms)\n"
 
 func main() {
 	var start time.Time
+	var end  time.Time
 	var ms_total float64
-	var ms_per_call float64
+	var ns_per_call int64
 
 	//
 	// "github.com/broady/gogeohash"
@@ -28,17 +29,19 @@ func main() {
 	for i := 0; i < num_loops; i++ {
 		geohash.Encode(latitude, longitude)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "encode", "gogeohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "encode", "gogeohash", ns_per_call, num_loops, ms_total)
 
 	start = time.Now()
 	for i := 0; i < num_loops; i++ {
 		geohash.Decode(geostr)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "decode", "gogeohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "decode", "gogeohash", ns_per_call, num_loops, ms_total)
 
 	//
 	// "github.com/gnagel/go-geohash"
@@ -47,31 +50,35 @@ func main() {
 	for i := 0; i < num_loops; i++ {
 		go_geohash.Encode(latitude, longitude, precision)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "encode", "go-geohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "encode", "go-geohash", ns_per_call, num_loops, ms_total)
 
 	start = time.Now()
 	for i := 0; i < num_loops; i++ {
 		go_geohash.Decode(geostr)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "decode", "go-geohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "decode", "go-geohash", ns_per_call, num_loops, ms_total)
 
 	start = time.Now()
 	for i := 0; i < num_loops; i++ {
 		go_geohash.DecodeBoundBox(geostr)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "decode_bbox", "go-geohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "decode_bbox", "go-geohash", ns_per_call, num_loops, ms_total)
 
 	start = time.Now()
 	for i := 0; i < num_loops; i++ {
 		go_geohash.Neighbor(neighbor, directions)
 	}
-	ms_total = float64(time.Now().Sub(start).Nanoseconds() / int64(1000000))
-	ms_per_call = ms_total / float64(num_loops)
-	fmt.Printf(format_tag, "neighbor", "go-geohash", ms_per_call, num_loops, ms_total)
+	end = time.Now()
+	ms_total = float64(end.Sub(start).Nanoseconds() / int64(1000000))
+	ns_per_call = end.Sub(start).Nanoseconds() / int64(num_loops)
+	fmt.Printf(format_tag, "neighbor", "go-geohash", ns_per_call, num_loops, ms_total)
 }
